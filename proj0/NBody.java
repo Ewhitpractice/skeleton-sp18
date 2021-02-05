5796d9f663294febb1e10e95b1a9ad44f51af4c0
Emily Whitney <emilywhitney@emilys-mbp.myfiosgateway.com>
2021-02-05 16:22:47 -0500

    linkedlistdeque and arraydeque

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,6 +6,7 @@ public class NBody
     public static double readRadius(String filename)
     {
         In in = new In(filename);
+        int i = in.readInt();
         double R = in.readDouble();
         return R;
     }
@@ -42,8 +43,51 @@ public class NBody
 
         /** drawing the background */
         String background = "images/starfield.jpg";
-        StdDraw.setScale(-R,R);
+        StdDraw.setXscale(-R,R);
+        StdDraw.setYscale(-R,R);
         StdDraw.picture(0,0,background);
 
+        /**drawing the planets */
+        for(int i=0; i<planets.length;i++)
+        {
+            planets[i].draw();
+        }
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.show();
+        double time = 0;
+        while(time != T)
+        {
+            double[] xForces = new double[planets.length];
+            double[] yForces = new double[planets.length];
+            for(int i=0; i<planets.length;i++)
+            {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for(int i=0; i<planets.length;i++)
+            {
+                planets[i].update(dt,xForces[i],yForces[i]);
+            }
+
+            StdDraw.picture(0,0,background);
+
+            for(int i=0; i<planets.length;i++)
+            {
+                planets[i].draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(10);
+            time = time +dt;
+        }
+
+        StdOut.printf("%d\n", planets.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i<planets.length;i++)
+        {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+            planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+        }
+        }
     }
 }
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,150 @@
+public class LinkedListDeque<T>
+{
+    private final ItemNode<T> sentinel;
+    private int size;
+
+    private static class ItemNode<T>
+    {
+        public ItemNode<T> prev; //arrow to the previous element
+        public T item; //the item stored in the list
+        public ItemNode<T> next; //arrow to the next element
+
+        public ItemNode(T x)
+        {
+            prev = null;
+            next = null;
+            item = x;
+        }
+    }
+
+    public LinkedListDeque()
+    {
+        sentinel = new ItemNode<>(null);
+        size = 0;
+    }
+
+    public void addFirst(T item)
+    {
+        ItemNode<T> first_item = new ItemNode<>(item);
+
+        if(size == 0)
+        {
+            sentinel.next = first_item;
+            first_item.prev = sentinel;
+            sentinel.prev = first_item;
+            first_item.next = sentinel;
+        }
+        ItemNode<T> second_item = sentinel.next;
+        first_item.next = second_item;
+        sentinel.next = first_item;
+        second_item.prev = first_item;
+        first_item.prev = sentinel;
+        size = size + 1;
+    }
+
+    public void addLast(T item)
+    {
+        ItemNode<T> last_item = new ItemNode<>(item);
+        if(size == 0)
+        {
+            sentinel.next = last_item;
+            last_item.prev = sentinel;
+            last_item.next = sentinel;
+            sentinel.prev = last_item;
+        }
+        ItemNode<T> second_to_last = sentinel.prev;
+        second_to_last.next = last_item;
+        last_item.prev = second_to_last;
+        last_item.next = sentinel;
+        sentinel.prev = last_item;
+        size = size + 1;
+    }
+
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    public int size()
+    {
+        return size;
+    }
+
+    public void printDeque()
+    {
+        ItemNode<T> pointer = new ItemNode<>(sentinel.next.item);
+        while(pointer != null)
+        {
+            String print_object = pointer.item.toString();
+            System.out.println(print_object);
+            pointer = pointer.next;
+        }
+    }
+
+    public T removeFirst()
+    {
+        T removed = sentinel.next.item;
+        ItemNode<T> to_remove = sentinel.next;
+        if(size == 0)
+        {
+            return null;
+        }
+        if(size == 1)
+        {
+            sentinel.next = null;
+            sentinel.prev = null;
+            size = 0;
+            return removed;
+        }
+        ItemNode<T> new_first = to_remove.next;
+        sentinel.next = new_first;
+        new_first.prev = sentinel;
+        to_remove.next = null;
+        to_remove.prev = null;
+        size = size - 1;
+        return removed;
+    }
+
+    public T removeLast()
+    {
+        T removed = sentinel.next.item;
+        ItemNode<T> to_remove = sentinel.next;
+        if(size == 0)
+        {
+            return null;
+        }
+        if(size == 1)
+        {
+            sentinel.next = null;
+            sentinel.prev = null;
+            size = 0;
+            return removed;
+        }
+        ItemNode<T> new_last = to_remove.prev;
+        sentinel.prev = new_last;
+        new_last.next = sentinel;
+        to_remove.next = null;
+        to_remove.prev = null;
+        size = size-1;
+        return removed;
+    }
+
+    public T get(int index)
+    {
+        ItemNode<T> pointer = new ItemNode<>(sentinel.next.item);
+        while(index > 0)
+        {
+            pointer = pointer.next;
+            index = index - 1;
+        }
+        return pointer.item;
+    }
+
+    public T getRecursive(int index, ItemNode<T> current_node)
+    {
+        if(index == 0) {
+            return sentinel.next.item;
+        }
+        return getRecursive(index-1,current_node.next);
+    }
+}
