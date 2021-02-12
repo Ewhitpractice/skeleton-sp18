d8a11f25769e2d0076e507977b27a26056ce3218
Ewhitpractice <73607613+Ewhitpractice@users.noreply.github.com>
2021-02-12 14:02:43 -0500

    Update TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -5,12 +5,13 @@ public class TestOffByOne {
     static CharacterComparator offByOne = new OffByOne();
 
     @Test
-    public void testEqualChars(){
-        assertTrue(offByOne.equalChars('a','b'));
-        assertFalse(offByOne.equalChars('x','x'));
-        assertFalse(offByOne.equalChars('A','a'));
-        assertTrue(offByOne.equalChars('c','d'));
-
+    public void testEqualChars() {
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertFalse(offByOne.equalChars('x', 'x'));
+        assertFalse(offByOne.equalChars('A', 'a'));
+        assertTrue(offByOne.equalChars('c', 'd'));
+        assertTrue(offByOne.equalChars('a','B'));
+        assertFalse(offByOne.equalChars('Z','['));
     }
 
 }
