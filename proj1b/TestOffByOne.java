e9bb41b080989204720fb3d3ca138be675be5183
Ewhitpractice <73607613+Ewhitpractice@users.noreply.github.com>
2021-02-12 15:18:05 -0500

    Update TestOffByOne.java

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -9,10 +9,14 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertFalse(offByOne.equalChars('x', 'x'));
         assertFalse(offByOne.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('z','a'));
+        assertFalse(offByOne.equalChars('a','g'));
         assertTrue(offByOne.equalChars('c', 'd'));
-        assertTrue(offByOne.equalChars('a','B'));
-        assertFalse(offByOne.equalChars('Z','['));
+        assertFalse(offByOne.equalChars('a','B'));
+        assertTrue(offByOne.equalChars('$','%'));
+        assertFalse(offByOne.equalChars('A','b'));
+        assertTrue(offByOne.equalChars('1','2'));
+        assertFalse(offByOne.equalChars('1','3'));
     }
 
 }
-
