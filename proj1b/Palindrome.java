cc707339d15b9e0ec0aa5b46e73d27f4c9d1e777
Ewhitpractice <73607613+Ewhitpractice@users.noreply.github.com>
2021-02-12 14:01:49 -0500

    Update Palindrome.java

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,24 +1,23 @@
 public class Palindrome {
-    public Deque<Character> wordToDeque(String word){
+    public Deque<Character> wordToDeque(String word) {
         Deque<Character> characters = new LinkedListDeque<>();
-        for(int i=0;i<word.length();i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             characters.addLast(word.charAt(i));
         }
         return characters;
     }
 
-    public boolean isPalindrome(String word){
+    public boolean isPalindrome(String word) {
         Deque<Character> forward = wordToDeque(word);
         Deque<Character> backward = new LinkedListDeque<>();
-        for(int i = 1; i < word.length()+1; i++)
+        for (int i = 1; i < word.length() + 1; i++)
         {
             backward.addLast(word.charAt(word.length()-i));
         }
 
-        for(int i = 0; i < word.length(); i++)
+        for (int i = 0; i < word.length(); i++)
         {
-            if(!forward.get(i).equals(backward.get(i)))
+            if (!forward.get(i).equals(backward.get(i)))
             {
                 return false;
             }
@@ -26,16 +25,16 @@ public class Palindrome {
         return true;
     }
 
-    public boolean isPalindrome(String word, CharacterComparator cc){
+    public boolean isPalindrome(String word, CharacterComparator cc) {
         CharacterComparator obo = new OffByOne();
         Deque<Character> forward = wordToDeque(word);
         Deque<Character> backward = new LinkedListDeque<Character>();
-        for(int i = 1; i < word.length()+1; i++)
+        for (int i = 1; i < word.length() + 1; i++)
         {
             backward.addLast(word.charAt(word.length()-i));
         }
-        for(int i=0;i<word.length();i++) {
-            if(i == word.length()/2) {
+        for (int i = 0; i < word.length(); i++) {
+            if (i == word.length() / 2) {
                 continue;
             }
             if (!obo.equalChars(forward.get(i), backward.get(i))) {
@@ -45,3 +44,4 @@ public class Palindrome {
         return true;
     }
 }
+
