b463ebb8105c4ec1d591041b25aa7cd23e83175c
Ewhitpractice <73607613+Ewhitpractice@users.noreply.github.com>
2021-02-12 11:16:50 -0500

    Update TestPalindrome.java

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,17 +2,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
-    // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+    static OffByOne obo = new OffByOne();
 
     @Test
     public void testWordToDeque() {
         Deque d = palindrome.wordToDeque("persiflage");
         String actual = "";
         for (int i = 0; i < "persiflage".length(); i++) {
-            actual += d.removeFirst();
+            actual = actual + d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+
+    @Test
+    public void testIsPalindrome(){
+        assertFalse(palindrome.isPalindrome("hello"));
+        assertTrue(palindrome.isPalindrome("noon"));
+        assertFalse(palindrome.isPalindrome("Nobody"));
+        assertTrue(palindrome.isPalindrome("H"));
+    }
+
+    @Test
+    public void testIsPalindromeTwo()
+    {
+        assertFalse(palindrome.isPalindrome("hello",obo));
+        assertTrue(palindrome.isPalindrome("ab",obo));
+        assertTrue(palindrome.isPalindrome("flake",obo));
+    }
+
 }
