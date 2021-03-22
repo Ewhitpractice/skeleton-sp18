130a281dde1d6e5051c8cc788999b44fe409a755
Ewhitpractice <emyw123@gmail.com>
2021-03-22 15:41:17 -0400

    finished hw2 (attempt 1)

--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -1,6 +1,9 @@
 package hw3.hash;
 
 import org.junit.Test;
+
+import javax.lang.model.util.SimpleElementVisitor6;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
@@ -29,6 +32,20 @@ public class TestSimpleOomage {
           meaning no two SimpleOomages should EVER have the same
           hashCode UNLESS they have the same red, blue, and green values!
          */
+        SimpleOomage ooA = new SimpleOomage(5,10,20);
+        int count = 0;
+        for (int i = 0; i < 256; i = i + 5) {
+            for (int j = 0; j < 256; j = j + 5) {
+                for (int k = 0; k < 256; k = k + 5) {
+                    SimpleOomage ooA2 = new SimpleOomage(i, j, k) ;
+                    if(ooA.hashCode() == ooA2.hashCode()) {
+                        count++;
+                    }
+
+                }
+            }
+        }
+        assertTrue(count == 1);
     }
 
     @Test
@@ -42,7 +59,7 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -50,10 +67,10 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
     /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -63,7 +80,7 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
